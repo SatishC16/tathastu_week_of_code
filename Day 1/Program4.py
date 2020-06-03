f4b6fbe4d55d7ff72620ce8aeb5a345c131c9ae9
Satish Chandan <53550303+SatishC16@users.noreply.github.com>
2020-06-03 20:40:26 +0530

    Create Program4.py

---
+++ Day 1/Program4.py
@@ -0,0 +1,7 @@
+cp = int(input("Enter cost-price: "))
+sp = int(input("Enter selling-price: "))
+prof = sp-cp
+print("Profit: Rs.",prof)
+
+nsp = prof*(1.05)
+print("Selling price should be Rs.",nsp,"to increase profit by 5%")
