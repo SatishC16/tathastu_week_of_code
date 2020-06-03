cb385cce95a476408978fdac287e6c8d5b68bf36
Satish Chandan <53550303+SatishC16@users.noreply.github.com>
2020-06-03 20:23:57 +0530

    Update Program1.py

--- Day 1/Program1.py
+++ Day 1/Program1.py
@@ -1,1 +1,10 @@
-hii
+name = input("Enter Name: ")
+college = input("Enter College Name: ")
+branch = input("Enter Branch: ")
+gender = input("Enter gender: ")
+age = int(input("Enter Age: "))
+print(name)
+print(college)
+print(branch)
+print(gender)
+print(age)
